bd07c47d49f45dc1ce56e6ae20bc76789d61def1
John Crepezzi <john.crepezzi@patch.com>
2010-03-30 14:43:10 -0400

    Bump version

--- lib/ice_cube.rb
+++ lib/ice_cube.rb
@@ -16,17 +16,18 @@ require 'ice_cube/rule'
 require 'ice_cube/schedule'
 require 'ice_cube/rule_occurrence'
 
-autoload :DailyRule, 'ice_cube/rules/daily_rule'
-autoload :WeeklyRule, 'ice_cube/rules/weekly_rule'
-autoload :MonthlyRule, 'ice_cube/rules/monthly_rule'
-autoload :YearlyRule, 'ice_cube/rules/yearly_rule'
-   
-autoload :HourlyRule, 'ice_cube/rules/hourly_rule'
-autoload :MinutelyRule, 'ice_cube/rules/minutely_rule'
-autoload :SecondlyRule, 'ice_cube/rules/secondly_rule'
-   
 module IceCube
-  VERSION = '0.1'
+  
+  autoload :DailyRule, 'ice_cube/rules/daily_rule'
+  autoload :WeeklyRule, 'ice_cube/rules/weekly_rule'
+  autoload :MonthlyRule, 'ice_cube/rules/monthly_rule'
+  autoload :YearlyRule, 'ice_cube/rules/yearly_rule'
+   
+  autoload :HourlyRule, 'ice_cube/rules/hourly_rule'
+  autoload :MinutelyRule, 'ice_cube/rules/minutely_rule'
+  autoload :SecondlyRule, 'ice_cube/rules/secondly_rule'
+
+  VERSION = '0.2'
 
   ONE_DAY = 24 * 60 * 60
   ONE_HOUR = 60 * 60
@@ -39,4 +40,5 @@ module IceCube
              :september => 9, :october => 10, :november => 11, :december => 12 }
   
   include TimeUtil
+
 end
