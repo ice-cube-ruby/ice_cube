0c8cf0250ec72e141e7c3914ba5c7ac086bc0064
John Crepezzi <john.crepezzi@patch.com>
2010-08-04 18:38:35 -0400

    Update README and add more specs for occurs_on?

--- spec/examples/ice_cube_spec.rb
+++ spec/examples/ice_cube_spec.rb
@@ -589,21 +589,51 @@ describe Schedule, 'occurs_on?' do
   end
 
   it 'should be able to work with occurs_on? at an odd time - start of day' do
-    schedule = Schedule.new(Time.local(2010, 1, 31, 0, 0, 0).in_time_zone('Pacific Time (US & Canada)'))
+    schedule = Schedule.new(Time.local(2010, 8, 10, 0, 0, 0).in_time_zone('Pacific Time (US & Canada)'))
     schedule.add_recurrence_rule Rule.weekly
-    schedule.occurs_on?(Date.new(2010, 1, 31)).should be(true)
-    schedule.occurs_on?(Date.new(2010, 1, 30)).should be(false)
-    schedule.occurs_on?(Date.new(2010, 2, 1)).should be(false)
-    schedule.occurs_on?(Date.new(2010, 2, 7)).should be(true)
+    schedule.occurs_on?(Date.new(2010, 8, 10)).should be(true)
+    schedule.occurs_on?(Date.new(2010, 8, 11)).should be(false)
+    schedule.occurs_on?(Date.new(2010, 8, 9)).should be(false)
+    schedule.occurs_on?(Date.new(2010, 8, 17)).should be(true)
   end
 
   it 'should be able to work with occurs_on? at an odd time - end of day' do
-    schedule = Schedule.new(Time.local(2010, 1, 31, 23, 59, 59).in_time_zone('Pacific Time (US & Canada)'))
+    schedule = Schedule.new(Time.local(2010, 8, 10, 23, 59, 59).in_time_zone('Pacific Time (US & Canada)'))
     schedule.add_recurrence_rule Rule.weekly
-    schedule.occurs_on?(Date.new(2010, 1, 31)).should be(true)
-    schedule.occurs_on?(Date.new(2010, 1, 30)).should be(false)
-    schedule.occurs_on?(Date.new(2010, 2, 1)).should be(false)
-    schedule.occurs_on?(Date.new(2010, 2, 7)).should be(true)
+    schedule.occurs_on?(Date.new(2010, 8, 10)).should be(true)
+    schedule.occurs_on?(Date.new(2010, 8, 11)).should be(false)
+    schedule.occurs_on?(Date.new(2010, 8, 9)).should be(false)
+    schedule.occurs_on?(Date.new(2010, 8, 17)).should be(true)
+  end
+
+  it 'should be able to work with occurs_on? at an odd time - start of day' do
+    schedule = Schedule.new(Time.local(2010, 8, 10, 0, 0, 0).in_time_zone('Pacific Time (US & Canada)'))
+    schedule.add_recurrence_date Time.local(2010, 8, 20, 0, 0, 0)
+    schedule.add_recurrence_rule Rule.weekly
+    schedule.occurs_on?(Date.new(2010, 8, 20)).should be(true)
+    schedule.occurs_on?(Date.new(2010, 8, 21)).should be(false)
+    schedule.occurs_on?(Date.new(2010, 8, 19)).should be(false)
+  end
+
+  it 'should be able to work with occurs_on? at an odd time - end of day' do
+    schedule = Schedule.new(Time.local(2010, 8, 10, 23, 59, 59).in_time_zone('Pacific Time (US & Canada)'))
+    schedule.add_recurrence_date Time.local(2010, 8, 20, 23, 59, 59)
+    schedule.occurs_on?(Date.new(2010, 8, 20)).should be(true)
+    schedule.occurs_on?(Date.new(2010, 8, 21)).should be(false)
+    schedule.occurs_on?(Date.new(2010, 8, 19)).should be(false)
+  end
+
+  it 'should work with occurs on for a single day schedule' do
+    time = Time.local(2010, 8, 12, 23, 0, 0)
+    date = Date.new(2010, 8, 12)
+    # build the schedule
+    schedule = Schedule.new(time)
+    schedule.add_recurrence_date time
+    schedule.all_occurrences.should == [time]
+    # test occurs_on? for surrounding dates
+    schedule.occurs_on?(date).should be(true)
+    schedule.occurs_on?(date + 1).should be(false)
+    schedule.occurs_on?(date - 1).should be(false)
   end
   
 end
