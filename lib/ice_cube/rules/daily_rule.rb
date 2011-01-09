09502c9dad5163c73a6ae04d6d7caeef8be19458
dave farkas <dave@interactivemediums.com>
2011-01-09 17:54:23 -0600

    fixed issue with a daily rule that had an hour of day and minute of hour

--- lib/ice_cube/rules/daily_rule.rb
+++ lib/ice_cube/rules/daily_rule.rb
@@ -6,32 +6,31 @@ module IceCube
     # Determine whether this rule occurs on a give date.
     def in_interval?(date, start_date)
       #make sure we're in a proper interval
-      day_count = ((date - start_date) / IceCube::ONE_DAY).to_i
+      day_count = date.yday - start_date.yday
       day_count % @interval == 0
-      true
     end
 
-    def to_ical 
+    def to_ical
       'FREQ=DAILY' << to_ical_base
     end
-       
+
     def to_s
       to_s_base 'Daily', "Every #{@interval} days"
     end
-        
+
     protected
-    
+
     def default_jump(date, attempt_count = nil)
       goal = date + IceCube::ONE_DAY * @interval
       adjust(goal, date)
     end
-     
+
     private
-    
+
     def initialize(interval)
       super(interval)
     end
-        
+
   end
 
 end
--- spec/examples/daily_rule_spec.rb
+++ spec/examples/daily_rule_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe IceCube::DailyRule, 'occurs_on?' do
-  
+
   it 'should produce the correct days for @interval = 1' do
     start_date = DAY
     schedule = IceCube::Schedule.new(start_date)
@@ -16,10 +16,21 @@ describe IceCube::DailyRule, 'occurs_on?' do
     start_date = DAY
     schedule = IceCube::Schedule.new(start_date)
     schedule.add_recurrence_rule IceCube::Rule.daily(2)
-    #check assumption (3) -- (1) 2 (3) 4 (5) 6 
+    #check assumption (3) -- (1) 2 (3) 4 (5) 6
     dates = schedule.occurrences(start_date + 5 * IceCube::ONE_DAY)
     dates.size.should == 3
     dates.should == [DAY, DAY + 2 * IceCube::ONE_DAY, DAY + 4 * IceCube::ONE_DAY]
   end
-    
+
+  it 'should produce the correct days for interval of 4 day with hour and minute of day set' do
+    start_date = DAY
+    schedule = IceCube::Schedule.new(start_date)
+    schedule.add_recurrence_rule IceCube::Rule.daily(4).hour_of_day(5).minute_of_hour(45)
+
+    #check assumption 2 -- 1 (2) (3) (4) 5 (6)
+    dates = schedule.occurrences(start_date + 5 * IceCube::ONE_DAY)
+    dates.size.should == 2
+    dates.should == [DAY + 5.hours + 45.minutes, DAY + 4 * IceCube::ONE_DAY + 5.hours + 45.minutes]
+  end
+
 end
