f077220366e628497f4d26a237e8b286f0460500
John Crepezzi <john.crepezzi@gmail.com>
2011-12-08 01:39:57 -0500

    Fix spec

--- lib/ice_cube/validations/lock.rb
+++ lib/ice_cube/validations/lock.rb
@@ -19,9 +19,27 @@ module IceCube
     # (meaning, its not a fixed interval)
     def validate_day_lock(time, schedule)
       start = value || schedule.start_time.day
-      days_in_month = TimeUtil.days_in_month(time)
-      start = days_in_month + start + 1 if start < 0 # handle negatives
-      start >= time.day ? start - time.day : days_in_month - time.day + start
+      days_in_this_month = TimeUtil.days_in_month(time)
+      # If this number is positive, then follow our normal procedure
+      if start > 0
+        return start >= time.day ? start - time.day : days_in_this_month - time.day + start
+      end 
+      # If the number is negative, and it resolved against the current month
+      # puts it in the future, just return the difference
+      days_in_this_month = TimeUtil.days_in_month(time)
+      start_one = days_in_this_month + start + 1 
+      if start_one >= time.day
+        return start_one - time.day
+      end
+      # Otherwise, we need to figure out the meaning of the value
+      # in the next month, and then figure out how to get there
+      days_in_next_month = TimeUtil.days_in_next_month(time)
+      start_two = days_in_next_month + start + 1
+      if start_two >= time.day
+        days_in_this_month + start_two - time.day
+      else
+        days_in_next_month + start_two - time.day
+      end
     end
 
   end
