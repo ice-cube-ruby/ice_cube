6daa4a35f0e66675baa2a59619a58d178b9f6a44
John Crepezzi <john.crepezzi@gmail.com>
2012-10-11 18:06:44 -0400

    Remove implicit time to_time

--- lib/ice_cube/schedule.rb
+++ lib/ice_cube/schedule.rb
@@ -179,7 +179,6 @@ module IceCube
 
     # Determine if the schedule is occurring at a given time
     def occurring_at?(time)
-      time=time.to_time
       if duration
         return false if exception_time?(time)
         occurs_between?(time - duration + 1, time)
