d6921ba23cd7fcf6b3bd53b318ac033e2866aee3
Nathan Youngman <git@nathany.com>
2012-10-15 18:18:20 -0600

    Should not skip the next month for start dates on the 31st.

--- spec/examples/monthly_rule_spec.rb
+++ spec/examples/monthly_rule_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe IceCube::MonthlyRule, 'occurs_on?' do
-  
+
   it 'should produce the correct number of days for @interval = 1' do
     start_date = DAY
     schedule = IceCube::Schedule.new(start_date)
@@ -40,7 +40,7 @@ describe IceCube::MonthlyRule, 'occurs_on?' do
     #check assumption (month 1 monday) (month 2 monday)
     schedule.occurrences(start_date + 50 * IceCube::ONE_DAY).size.should == 2
   end
-  
+
   it 'should produce the correct number of days for @interval = 1 with only the last mondays' do
     start_date = Time.utc(2010, 1, 1)
     schedule = IceCube::Schedule.new(start_date)
@@ -58,4 +58,20 @@ describe IceCube::MonthlyRule, 'occurs_on?' do
     schedule.occurrences(end_date).size.should == 24
   end
 
+  it 'should produce dates on a monthly interval for the last day of the month' do
+    start_date = Time.utc(2010, 3, 31, 0, 0, 0)
+    schedule = IceCube::Schedule.new(start_date)
+    schedule.add_recurrence_rule IceCube::Rule.monthly
+    schedule.first(10).should == [Time.utc(2010, 3, 31, 0, 0, 0), Time.utc(2010, 4, 30, 0, 0, 0), Time.utc(2010, 5, 31, 0, 0, 0),
+                                  Time.utc(2010, 6, 30, 0, 0, 0), Time.utc(2010, 7, 31, 0, 0, 0), Time.utc(2010, 8, 31, 0, 0, 0),
+                                  Time.utc(2010, 9, 30, 0, 0, 0), Time.utc(2010, 10, 31, 0, 0, 0), Time.utc(2010, 11, 30, 0, 0, 0),
+                                  Time.utc(2010, 12, 31, 0, 0, 0)]
+  end
+
+  it 'should produce dates on a monthly interval for latter days in the month near February' do
+    start_date = Time.utc(2010, 1, 29, 0, 0, 0)
+    schedule = IceCube::Schedule.new(start_date)
+    schedule.add_recurrence_rule IceCube::Rule.monthly
+    schedule.first(3).should == [Time.utc(2010, 1, 29, 0, 0, 0), Time.utc(2010, 2, 28, 0, 0, 0), Time.utc(2010, 3, 29, 0, 0, 0)]
+  end
 end
