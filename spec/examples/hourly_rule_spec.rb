ca035cf2869b511b189e3affad1b7ed5b5b41113
Andrew Vit <andrew@avit.ca>
2013-02-17 14:47:25 -0800

    Clean up specs

--- spec/examples/hourly_rule_spec.rb
+++ spec/examples/hourly_rule_spec.rb
@@ -1,16 +1,19 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe IceCube::HourlyRule do
+module IceCube
+  describe HourlyRule do
 
-  it 'should produce the correct days for @interval = 3' do
-    start_date = DAY
-    schedule = IceCube::Schedule.new(start_date)
-    schedule = IceCube::Schedule.from_yaml(schedule.to_yaml)
-    schedule.add_recurrence_rule IceCube::Rule.hourly(3)
-    #check assumption (3) -- (1) 2 (3) 4 (5) 6
-    dates = schedule.first(3)
-    dates.size.should == 3
-    dates.should == [DAY, DAY + 3 * IceCube::ONE_HOUR, DAY + 6 * IceCube::ONE_HOUR]
+
+    it 'should produce the correct days for @interval = 3' do
+      start_date = DAY
+      schedule = Schedule.new(start_date)
+      schedule = Schedule.from_yaml(schedule.to_yaml)
+      schedule.add_recurrence_rule Rule.hourly(3)
+      #check assumption (3) -- (1) 2 (3) 4 (5) 6
+      dates = schedule.first(3)
+      dates.size.should == 3
+      dates.should == [DAY, DAY + 3 * ONE_HOUR, DAY + 6 * ONE_HOUR]
+    end
+
   end
-
 end
--- spec/examples/weekly_rule_spec.rb
+++ spec/examples/weekly_rule_spec.rb
@@ -1,93 +1,83 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe IceCube::WeeklyRule, 'occurs_on?' do
+module IceCube
+  describe WeeklyRule do
 
-  it 'should produce the correct number of days for @interval = 1 with no weekdays specified' do
-    start_date = Time.now
-    schedule = IceCube::Schedule.new(start_date)
-    schedule.add_recurrence_rule IceCube::Rule.weekly
-    #check assumption (2 weeks in the future) (1) (2) (3) (4) (5)
-    dates = schedule.occurrences(start_date + (7 * 3 + 1) * IceCube::ONE_DAY)
-    dates.size.should == 4
-  end
 
-  it 'should produce the correct number of days for @interval = 1 with only weekends' do
-    start_date = WEDNESDAY
-    schedule = IceCube::Schedule.new(start_date)
-    schedule.add_recurrence_rule IceCube::Rule.weekly.day(:saturday, :sunday)
-    #check assumption
-    schedule.occurrences(start_date + 7 * 4 * IceCube::ONE_DAY).size.should == 8
-  end
 
-  it 'should produce the correct number of days for @interval = 2 with only one day per week' do
-    start_date = WEDNESDAY
-    schedule = IceCube::Schedule.new(start_date)
-    schedule.add_recurrence_rule IceCube::Rule.weekly(2).day(:wednesday)
-    #check assumption
-    dates = schedule.occurrences(start_date + 7*3*IceCube::ONE_DAY)
-    dates.should == [start_date, start_date + 14*IceCube::ONE_DAY]
-  end
-
-  it 'should produce the correct days for @interval = 2, regardless of the start week' do
-    start_date = WEDNESDAY + 7*IceCube::ONE_DAY
-    schedule = IceCube::Schedule.new(start_date)
-    schedule.add_recurrence_rule IceCube::Rule.weekly(2).day(:wednesday)
-    #check assumption
-    dates = schedule.occurrences(start_date + 7*3*IceCube::ONE_DAY)
-    dates.should == [start_date, start_date + 14*IceCube::ONE_DAY]
-  end
-
-  it 'should occurr every 2nd tuesday of a month' do
-    now = Time.now
-    schedule = IceCube::Schedule.new(Time.local(now.year, now.month, now.day))
-    schedule.add_recurrence_rule IceCube::Rule.monthly.hour_of_day(11).day_of_week(:tuesday => [2])
-    schedule.first(100).each do |d|
-      d.hour.should == 11
-      d.wday.should == 2
+    it 'should produce the correct number of days for @interval = 1 with no weekdays specified' do
+      schedule = Schedule.new(t0 = Time.now)
+      schedule.add_recurrence_rule Rule.weekly
+      #check assumption (2 weeks in the future) (1) (2) (3) (4) (5)
+      times = schedule.occurrences(t0 + (7 * 3 + 1) * ONE_DAY)
+      times.size.should == 4
     end
-  end
 
-  it 'should occur on every first day of a month at midnight and not skip months when DST ends' do
-    start_date = Time.local(2011, 8, 1)
-    [:sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday].each_with_index do |day, index|
-      schedule = IceCube::Schedule.new(start_date)
-      schedule.add_recurrence_rule IceCube::Rule.monthly.day_of_week(day => [1])
-      last_date = nil
-      schedule.first(100).each do |current_date|
-        # should be the correct day of week
-        current_date.wday.should == index
-        # should be midnight
-        current_date.hour.should == 0
-        if last_date then
-          month_interval = (current_date.year * 12 + current_date.month) - (last_date.year * 12 + last_date.month)
-          # should not skip months
-          month_interval.should == 1
-        end
-        last_date = current_date
+    it 'should produce the correct number of days for @interval = 1 with only weekends' do
+      schedule = Schedule.new(t0 = WEDNESDAY)
+      schedule.add_recurrence_rule Rule.weekly.day(:saturday, :sunday)
+      #check assumption
+      schedule.occurrences(t0 + 4 * ONE_WEEK).size.should == 8
+    end
+
+    it 'should produce the correct number of days for @interval = 2 with only one day per week' do
+      schedule = Schedule.new(t0 = WEDNESDAY)
+      schedule.add_recurrence_rule Rule.weekly(2).day(:wednesday)
+      #check assumption
+      times = schedule.occurrences(t0 + 3 * ONE_WEEK)
+      times.should == [t0, t0 + 2 * ONE_WEEK]
+    end
+
+    it 'should produce the correct days for @interval = 2, regardless of the start week' do
+      schedule = Schedule.new(t0 = WEDNESDAY + ONE_WEEK)
+      schedule.add_recurrence_rule Rule.weekly(2).day(:wednesday)
+      #check assumption
+      times = schedule.occurrences(t0 + 3 * ONE_WEEK)
+      times.should == [t0, t0 + 2 * ONE_WEEK]
+    end
+
+    it 'should occur every 2nd tuesday of a month' do
+      schedule = Schedule.new(t0 = Time.now)
+      schedule.add_recurrence_rule Rule.monthly.hour_of_day(11).day_of_week(:tuesday => [2])
+      schedule.first(48).each do |d|
+        d.hour.should == 11
+        d.wday.should == 2
       end
     end
-  end
 
-  it 'should be able to start on sunday but repeat on wednesdays' do
-    schedule = IceCube::Schedule.new(Time.local(2010, 8, 1))
-    schedule.add_recurrence_rule IceCube::Rule.weekly.day(:monday)
-    schedule.first(3).should == [Time.local(2010, 8, 2), Time.local(2010, 8, 9), Time.local(2010, 8, 16)]
-  end
+    it 'should be able to start on sunday but repeat on wednesdays' do
+      schedule = Schedule.new(t0 = Time.local(2010, 8, 1))
+      schedule.add_recurrence_rule Rule.weekly.day(:monday)
+      schedule.first(3).should == [
+        Time.local(2010, 8,  2),
+        Time.local(2010, 8,  9),
+        Time.local(2010, 8, 16)
+      ]
+    end
 
-  it 'should start weekly rules on monday when monday is the week start' do
-    schedule = IceCube::Schedule.new(Time.local(2012,2,7))
-    schedule.add_recurrence_rule IceCube::Rule.weekly(2, :monday).day(:tuesday, :sunday)
-    schedule.first(3).should == [Time.local(2012,2,7), Time.local(2012,2,12), Time.local(2012,2,21)]
-  end
+    it 'should start weekly rules on monday when monday is the week start' do
+      schedule = Schedule.new(t0 = Time.local(2012, 2, 7))
+      schedule.add_recurrence_rule Rule.weekly(2, :monday).day(:tuesday, :sunday)
+      schedule.first(3).should == [
+        Time.local(2012, 2,  7),
+        Time.local(2012, 2, 12),
+        Time.local(2012, 2, 21)
+      ]
+    end
 
-  it 'should start weekly rules on sunday by default' do
-    schedule = IceCube::Schedule.new(Time.local(2012,2,7))
-    schedule.add_recurrence_rule IceCube::Rule.weekly(2).day(:tuesday, :sunday)
-    schedule.first(3).should == [Time.local(2012,2,7), Time.local(2012,2,19), Time.local(2012,2,21)]
-  end
+    it 'should start weekly rules on sunday by default' do
+      schedule = Schedule.new(t0 = Time.local(2012,2,7))
+      schedule.add_recurrence_rule Rule.weekly(2).day(:tuesday, :sunday)
+      schedule.first(3).should == [
+        Time.local(2012, 2,  7),
+        Time.local(2012, 2, 19),
+        Time.local(2012, 2, 21)
+      ]
+    end
 
-  it 'should validate week_start input' do
-    expect { IceCube::Rule.weekly(2, :someday) }.to raise_error
-  end
+    it 'should validate week_start input' do
+      expect { Rule.weekly(2, :someday) }.to raise_error
+    end
 
+  end
 end
