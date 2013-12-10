5b913e40cb68251439d31e9e8d86afb713c8123d
John Crepezzi <john.crepezzi@gmail.com>
2013-12-10 16:58:21 -0500

    Time.now with no usec in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,18 @@ require File.dirname(__FILE__) + '/../lib/ice_cube'
 
 DAY = Time.utc(2010, 3, 1)
 WEDNESDAY = Time.utc(2010, 6, 23, 5, 0, 0)
+
+# In tests, make Time.now have no usec so our post-serialization comparisons
+# don't break
+class Time
+
+  class << self
+    alias :now_original :now
+  end
+
+  def self.now
+    t = now_original
+    Time.new(t.year, t.month, t.day, t.hour, t.min, t.sec)
+  end
+
+end
