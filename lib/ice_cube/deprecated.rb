1640a94735f5f59e165c34759dfb7f67cc1ed6b5
Tyler Pickett <t.pickett66@gmail.com>
2014-05-02 07:52:09 -0600

    normalize whitespace in deprecated module
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/ice_cube/deprecated.rb
+++ lib/ice_cube/deprecated.rb
@@ -1,30 +1,30 @@
 module IceCube
-	module Deprecated
+  module Deprecated
 
-		# Define a deprecated alias for a method
-		# @param [Symbol] name - name of method to define
-		# @param [Symbol] replacement - name of method to replace (alias)
-		def deprecated_alias(name, replacement)
-			# Create a wrapped version
-			define_method(name) do |*args, &block|
-				warn "IceCube: #{self.class}##{name} is deprecated, please use ##{replacement} at: #{ caller[0] }"
-				send replacement, *args, &block
-			end
-		end
+    # Define a deprecated alias for a method
+    # @param [Symbol] name - name of method to define
+    # @param [Symbol] replacement - name of method to replace (alias)
+    def deprecated_alias(name, replacement)
+      # Create a wrapped version
+      define_method(name) do |*args, &block|
+        warn "IceCube: #{self.class}##{name} is deprecated, please use ##{replacement} at: #{ caller[0] }"
+        send replacement, *args, &block
+      end
+    end
 
-		# Deprecate a defined method
-		# @param [Symbol] name - name of deprecated method
-		# @param [Symbol] replacement - name of the desired replacement
-		def deprecated(name, replacement)
-			# Replace old method
-			old_name = :"#{name}_without_deprecation"
-			alias_method old_name, name
-			# And replace it with a wrapped version
-			define_method(name) do |*args, &block|
-				warn "IceCube: #{self.class}##{name} is deprecated, please use ##{replacement} at: #{ caller[0] }"
-				send old_name, *args, &block
-			end
-		end
+    # Deprecate a defined method
+    # @param [Symbol] name - name of deprecated method
+    # @param [Symbol] replacement - name of the desired replacement
+    def deprecated(name, replacement)
+      # Replace old method
+      old_name = :"#{name}_without_deprecation"
+      alias_method old_name, name
+      # And replace it with a wrapped version
+      define_method(name) do |*args, &block|
+        warn "IceCube: #{self.class}##{name} is deprecated, please use ##{replacement} at: #{ caller[0] }"
+        send old_name, *args, &block
+      end
+    end
 
     def self.schedule_options(schedule, options)
       if options[:start_date_override]
@@ -32,6 +32,5 @@ module IceCube
         schedule.start_time = options[:start_date_override]
       end
     end
-
-	end
+  end
 end
