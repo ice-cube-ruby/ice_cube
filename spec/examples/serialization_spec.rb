43f5aa90dee18d51fca68d61c200dd65946ce05f
Andrew Vit <andrew@avit.ca>
2014-05-24 11:47:12 -0700

    Avoid serializing YAML for blank values
    When dumping to the database, ActiveRecord would run blank values (nil
    and "") through the serializer and wrap it a YAML string that is not
    valid for us to parse as a schedule. When the attribute is blank going
    into the serialized format, it should remain blank coming back out.
    
    Fixed #231

--- spec/examples/serialization_spec.rb
+++ spec/examples/serialization_spec.rb
@@ -2,29 +2,45 @@ require 'active_support/time'
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe IceCube::Schedule do
+
   let(:schedule) { IceCube::Schedule.new Time.now }
   let(:yaml)     { described_class.dump(schedule) }
 
-  describe '::dump(schedule)' do
-    it 'serializes a Schedule object as YAML string' do
-      yaml.should be_a_kind_of String
+  describe "::dump(schedule)" do
+
+    it "serializes a Schedule object as YAML string" do
+      yaml.should start_with "---\n"
     end
+
+    [nil, ""].each do |blank|
+      context "when schedule is #{blank.inspect}" do
+        let(:schedule) { blank }
+
+        it "returns #{blank.inspect}" do
+          yaml.should be blank
+        end
+      end
+    end
+
   end
 
-  describe '::load(yaml)' do
+  describe "::load(yaml)" do
     let(:new_schedule) { described_class.load yaml }
 
-    it 'creates a new object from a YAML string' do
+    it "creates a new object from a YAML string" do
       new_schedule.start_time.to_s.should eq schedule.start_time.to_s
     end
 
-    context 'when yaml is blank' do
-      let(:yaml) { nil }
+    [nil, ""].each do |blank|
+      context "when yaml is #{blank.inspect}" do
+        let(:yaml) { blank }
 
-      it 'returns nil' do
-        new_schedule.should be_nil
+        it "returns #{blank.inspect}" do
+          new_schedule.should be blank
+        end
       end
     end
+
   end
 end
 
