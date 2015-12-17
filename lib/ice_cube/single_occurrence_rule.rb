90e61867b588b442fdea093221c4d33f6821a2f2
lilole <dan@danamis.com>
2015-12-17 16:12:01 -0600

    Support whole days, with no HHMMSS part, in RDATE and EXDATE lines

--- lib/ice_cube/single_occurrence_rule.rb
+++ lib/ice_cube/single_occurrence_rule.rb
@@ -13,6 +13,20 @@ module IceCube
       true
     end
 
+    # Override from Rule, so any RDATE or EXDATE values may be used without HH:MM:SS defined.
+    #
+    def on?(time, schedule)
+      next_time(time, schedule, time).to_i == time.to_i || whole_day? && same_day?(time)
+    end
+
+    def whole_day?
+      time.hour == 0 && time.min == 0 && time.sec == 0
+    end
+
+    def same_day?(other)
+      time.year == other.year && time.month == other.month && time.day == other.day
+    end
+
     def next_time(t, schedule, closing_time)
       unless closing_time && closing_time < t
         time if time.to_i >= t.to_i
